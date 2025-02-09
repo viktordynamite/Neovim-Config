344d9aa1702bbd22985b20c98b5e7a49f92e8bb1
DB <dvalitins@gmail.com>
2025-02-10 00:32:13 +0200

    Started integrating snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000, -- Ensure it loads early if needed
+  lazy = false,    -- Load it immediately
+  opts = {
+    -- Enable specific features (customize as needed)
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  }
+}
+
